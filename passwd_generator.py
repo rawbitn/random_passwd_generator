ec25065671cdbba5c76317078820f3328369c0a7
rawbitn <108452658+rawbitn@users.noreply.github.com>
2022-07-03 21:56:05 +0530

    Add files via upload

---
+++ passwd_generator.py
@@ -0,0 +1,18 @@
+import random
+import datetime
+
+def generate_password(character_set):
+    password = ''
+    character_file = open(character_set, 'r+')
+    character_array = character_file.readlines()
+    len_character_array = len(character_array)
+    for i in range(0, 8):
+        raw = random.randint(0, len_character_array-1)
+        password = str(password) + str(character_array[raw].rstrip('\n'))
+    character_file.close()
+    return password
+
+#Example
+
+file_name = "characters.txt"    # file name which contains the character list
+print(generate_password(file_name))
